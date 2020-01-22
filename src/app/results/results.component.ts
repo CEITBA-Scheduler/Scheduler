9d688c7cee9ede3d6863da902fbd6909bc691a82
Ariel Nowik <newtonis.penguin@gmail.com>
2020-01-22 01:05:33 -0300

    add

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { SubjectCommissions } from '../materia';
+import { Subject, SubjectCommissions } from '../materia';
 import { Observable, BehaviorSubject } from 'rxjs';
+import { SgaLinkerService } from '../sga-linker.service';
 
 @Component({
   selector: 'app-results',
@@ -9,11 +10,34 @@ import { Observable, BehaviorSubject } from 'rxjs';
 })
 export class ResultsComponent implements OnInit {
   commissionsTest: Observable<SubjectCommissions[]>;
+  subjectsBehavioural: BehaviorSubject<SubjectCommissions[]> = new BehaviorSubject([]);
 
-  constructor() { }
+  constructor(private sgaLinkerService: SgaLinkerService) { }
 
   ngOnInit() {
-    this.commissionsTest = new BehaviorSubject<SubjectCommissions[]>([]).asObservable();
+
+    this.commissionsTest = this.subjectsBehavioural.asObservable();
+    
+    this.sgaLinkerService.getDataFromApi();
+    //var subjectsData: Subject[] = 
+    this.sgaLinkerService.getAllSubjects().subscribe(
+      (data: { [id: string]: Subject; }) => {
+        //console.log("data = ");
+        //console.log(data);
+
+        var subjectCommissionsTest: SubjectCommissions[] = [];
+
+        if (Object.keys(data).length > 0){
+          subjectCommissionsTest.push(
+            {subject: data["93.02"], commissions: [data["93.02"].commissions[0]]},
+            {subject: data["93.03"], commissions: [data["93.03"].commissions[2]]},
+            {subject: data["93.18"], commissions: [data["93.18"].commissions[1]]},
+            {subject: data["61.19"], commissions: [data["61.19"].commissions[0]]}
+          )
+
+        }
+        this.subjectsBehavioural.next(subjectCommissionsTest);
+      });
   }
 
 }
