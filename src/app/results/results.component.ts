de0bba318024390e95cf3379ba9efa30c3275062
juani <jualezama@itba.edu.ar>
2020-01-23 14:19:35 -0300

    Test

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -19,6 +19,7 @@ export class ResultsComponent implements OnInit {
 
   subjectsBehavioural: BehaviorSubject<SubjectCommissions[]>[] = [];
 
+  
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   combinations: Combination[];
 
