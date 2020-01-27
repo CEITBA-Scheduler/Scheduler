0e167742aeca83c60e179d0709e9af877f79bc93
jmestanza <jmestanza@itba.edu.ar>
2020-01-27 00:33:54 -0300

    arregle merge

--- src/app/combiner/combiner.component.ts
+++ src/app/combiner/combiner.component.ts
@@ -127,11 +127,7 @@ export class CombinerComponent implements OnInit {
     // Here, we take the other priorities from the general purpose service
     // and because the user interface has been simplified we use some
     // default values for the priority's settings
-<<<<<<< HEAD
-    const DEFAULT_SUPERPOSITION = 6.0;    // max amount of overlapped hours
-=======
     const DEFAULT_SUPERPOSITION = 4.0;    // max amount of overlapped hours
->>>>>>> a17f1167650d92fca8302ca489b502044728da65
     const DEFAULT_FREEDAY = Weekday.ANY;  // which freeday
     const DEFAULT_TRAVEL = 1.0;
     const userPreferences = this.generalProgramService.getAllCheckboxStatus();
